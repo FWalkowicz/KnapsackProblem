afb33352bc4c3d7281d2c5fc94fb864e9b788d58
kosiarn <kosiarn@users.noreply.github.com>
2023-03-30 16:32:55 +0200

    add unit tests

---
+++ Programowanie dynamiczne/dynamic_solution_test.py
@@ -0,0 +1,30 @@
+from dynamic_solution import construct_optimal_solution
+
+def presentation_problem_solution_should_return_296():
+    max_weight = 20
+    item_weight = [5, 7, 14, 5, 14, 11, 10, 11, 7, 6]
+    item_worth = [75, 100, 94, 41, 79, 114, 106, 94, 117, 79]
+    answer, matrix = construct_optimal_solution(max_weight, item_worth, item_weight)
+    if answer == 296:
+        return print("Passed!")
+    for row in matrix:
+        print(row)
+    return print(f"not passed! returned {answer} instead of 296")
+
+
+def optimal_solution_test(max_weight: list[int], item_weight: list[int], item_worth: list[int], correct_answer: int):
+    answer, matrix = construct_optimal_solution(max_weight, item_worth, item_weight)
+    if answer == correct_answer:
+        return print("Passed!")
+    for row in matrix:
+        print(row)
+    return print(f"not passed! returned {answer} instead of {correct_answer}")
+
+if __name__ == "__main__":
+    presentation_problem_solution_should_return_296()
+    optimal_solution_test(
+        6,
+        [1, 2, 3],
+        [10, 15, 40],
+        65
+    )
