2cf5d3c49f76197aec359b7ba161582bf95b64c4
kosiarn <kosiarn@users.noreply.github.com>
2023-03-30 17:13:27 +0200

    add reconstrucion func stub

--- Programowanie dynamiczne/dynamic_solution.py
+++ Programowanie dynamiczne/dynamic_solution.py
@@ -7,7 +7,8 @@ def fill_optimal_solution_row(
             previous_row[w]
             if item_weight[i - 1] > w
             else max(
-                previous_row[w], item_worth[i - 1] + previous_row[w - item_weight[i - 1]]
+                previous_row[w],
+                item_worth[i - 1] + previous_row[w - item_weight[i - 1]],
             )
         )
     return current_row
@@ -23,22 +24,33 @@ def construct_optimal_solution(
     `knapsack_weight_limit`: maksymalna waga zawartości plecaka
     `item_worth`: lista wartości rzeczy, które możemy włożyć do plecaka
     `item_weight`: lista wag rzeczy, które można włożyć do plecaka.
-                            Długość powinna być taka sama co item_worth.
+                            Długość i kolejność wartości powinna być zgodna z item_worth.
     """
     if len(item_worth) != len(item_weight):
         raise ValueError("Listy wartości i ciężarów mają różne długości.")
     n = len(item_worth)
     W_plus_one = knapsack_weight_limit + 1
     solution_matrix = [[0 for j in range(W_plus_one)] for k in range(n + 1)]
-    for i in range(
-        1, n + 1
-    ):  # zerowy wiersz zawiera same zera(z założenia algorytmu)
+    for i in range(1, n + 1):  # zerowy wiersz zawiera same zera(z założenia algorytmu)
         solution_matrix[i] = fill_optimal_solution_row(
             solution_matrix[i - 1], item_weight, item_worth, i
         )
     return (
-        solution_matrix[len(solution_matrix) - 1][len(solution_matrix[0]) -1],
+        solution_matrix[len(solution_matrix) - 1][len(solution_matrix[0]) - 1],
         solution_matrix,
     )
 
-#TODO add solution reconstruction
+
+
+
+
+def reconstruct_optimal_solution(solution_matrix: tuple[tuple[int]]):
+    value_change_register_matrix = [
+        [0 for w in range(len(solution_matrix[0]))] for i in range(len(solution_matrix))
+    ]
+    for i in range(1, len(value_change_register_matrix)):
+        for w in len(value_change_register_matrix[0]):
+            value_change_register_matrix[i][w] = (
+                0 if solution_matrix[i][w] == solution_matrix[i - 1][w] else 1
+            )
+    raise NotImplementedError
