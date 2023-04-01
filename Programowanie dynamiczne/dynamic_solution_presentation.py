7ce4f7c57116e1b99fa031afa4700567edb2043a
kosiarn <62908964+kosiarn@users.noreply.github.com>
2023-04-01 19:20:15 +0200

    add presentation script

---
+++ Programowanie dynamiczne/dynamic_solution_presentation.py
@@ -0,0 +1,21 @@
+from dynamic_solution import solve_01_knapsack
+
+knapsack_limit = 101
+show_matrix = False
+
+values = [79, 32, 47, 18, 26, 85, 33, 40, 45, 59]
+weights = [85, 26, 48, 21, 22, 95, 43, 45, 55, 52]
+
+solution = solve_01_knapsack(knapsack_limit, values, weights)
+
+print(
+    f"Optymalna wartość plecaka: {solution[0]}",
+    "Optymalna zawartość plecaka:",
+    f"Wartości: {solution[2]}",
+    f"Wagi:     {solution[3]}",  # yes, i did align the text with spaces
+    sep="\n",
+)
+if show_matrix == True:
+    print("Macierz optymalnego rozwiązania:")
+    for row in solution[1]:
+        print(row)
